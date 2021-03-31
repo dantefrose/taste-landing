905cb8cd242236df54d1b7500c5462c03371fa4c
Jeffrey Baucom <kishi@Jeffreys-MBP.attlocal.net>
2021-03-31 14:07:38 -0400

    initialize secret recipe page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 import "./mystyles.scss"
+import poster1 from "../images/poster1.png"
 
 const Index = () => (
   <div className="container">
     <div className="columns">
       <div className="left-menu column">
-        <div className="subtitle is-1">
+        <div className="subtitle is-2">
           <Link to="/ingredients">Ingredients</Link>
         </div>
-        <div className="subtitle is-1">
+        <div className="subtitle is-2">
           <a href="https://onlydfr.creator-spring.com/" >Take Home Plate</a>
         </div>
-        <div className="subtitle is-1">
+        <div className="subtitle is-2">
           <Link to="/secret-recipe">Secret Recipe</Link>
         </div>
       </div>
       <div className="cast-photo column">
-        <img src="https://via.placeholder.com/500x900" />
+        <img src={poster1} />
       </div>
       <div className="right-menu column">
         <div className="subtitle is-2">Social Media Links</div>
--- src/pages/secret-recipe.js
+++ src/pages/secret-recipe.js
@@ -1,8 +1,44 @@
 import React from "react"
 export default function Ingredients() {
   return (
-      <div className="container">
+    <div className="container">
+      <div className="columns">
+        <div className="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
+
           <div className="title is-1">Secret Recipe</div>
+          <form>
+            <div className="field">
+              <label className="label">1 tablespoon</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">2 cups</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">diced</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">pinch of</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="" />
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <button className="button is-link is-light">STIR</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
+    </div>
   )
 }
