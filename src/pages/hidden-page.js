04cb017f6f43d57f35163f53ecbf1cb932b2e148
Jeffrey Baucom <kishi@Jeffreys-MBP.attlocal.net>
2021-04-01 16:21:38 -0400

    add form and hidden page with navigation

---
+++ src/pages/hidden-page.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function HiddenPage() {
+  return (
+      <div className="container">
+          <div className="title is-1">Hidden Page</div>
+      </div>
+  )
+}
--- src/pages/secret-recipe.js
+++ src/pages/secret-recipe.js
@@ -1,39 +1,79 @@
 import React from "react"
-export default function Ingredients() {
+import { useState } from "react"
+import { navigate, Navigate } from "gatsby"
+
+function SecretRecipe() {
+  const [tablespoon, setTablespon] = useState("")
+  const [cups, setCups] = useState("")
+  const [diced, setDiced] = useState("")
+  const [pinch, setPinch] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const input = `${tablespoon}${cups}${diced}${pinch}`
+    console.log(input)
+    if (input === "1234") {
+      navigate('/hidden-page/')
+    }
+  }
   return (
     <div className="container">
       <div className="columns">
         <div className="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
 
           <div className="title is-1">Secret Recipe</div>
-          <form>
+          <form onSubmit={e => {handleSubmit(e)}}>
             <div className="field">
               <label className="label">1 tablespoon</label>
               <div className="control">
-                <input className="input" type="text" placeholder="" />
+                <input
+                  className="input"
+                  type="text"
+                  name="tablespoon"
+                  value={tablespoon}
+                  onChange={e => setTablespon(e.target.value)}
+                  />
               </div>
             </div>
             <div className="field">
               <label className="label">2 cups</label>
               <div className="control">
-                <input className="input" type="text" placeholder="" />
+                <input
+                  className="input"
+                  type="text"
+                  name="cups"
+                  value={cups}
+                  onChange={e => setCups(e.target.value)}
+                  />
               </div>
             </div>
             <div className="field">
               <label className="label">diced</label>
               <div className="control">
-                <input className="input" type="text" placeholder="" />
+                <input
+                  className="input"
+                  type="text"
+                  name="diced"
+                  value={diced}
+                  onChange={e => setDiced(e.target.value)}
+                  />
               </div>
             </div>
             <div className="field">
               <label className="label">pinch of</label>
               <div className="control">
-                <input className="input" type="text" placeholder="" />
+                <input
+                  className="input"
+                  type="text"
+                  name="pinch"
+                  value={pinch}
+                  onChange={e => setPinch(e.target.value)}
+                  />
               </div>
             </div>
             <div className="field">
               <div className="control">
-                <button className="button is-link is-light">STIR</button>
+                <button type="submit" className="button is-link is-light">STIR</button>
               </div>
             </div>
           </form>
@@ -42,3 +82,4 @@ export default function Ingredients() {
     </div>
   )
 }
+export default SecretRecipe
