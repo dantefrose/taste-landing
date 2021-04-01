a268e3dc3b526497268c61e38a267f54387cd974
Jeffrey Baucom <kishi@Jeffreys-MBP.attlocal.net>
2021-04-01 15:50:31 -0400

    add icons fix some styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 import "./mystyles.scss"
-import poster1 from "../images/poster1.png"
+import poster2 from "../images/poster2.png"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+library.add(fab);
 
 const Index = () => (
   <div className="container">
     <div className="columns">
-      <div className="left-menu column">
+      <div className="left-menu column is-one-third">
         <div className="subtitle is-2">
           <Link to="/ingredients">Ingredients</Link>
         </div>
@@ -17,11 +22,19 @@ const Index = () => (
           <Link to="/secret-recipe">Secret Recipe</Link>
         </div>
       </div>
-      <div className="cast-photo column">
-        <img src={poster1} />
+      <div className="cast-photo column is-one-third">
+        <img className="image is-fullwidth" src={poster2} />
       </div>
-      <div className="right-menu column">
-        <div className="subtitle is-2">Social Media Links</div>
+      <div className="right-menu column is-one-third">
+          <div className="right-icon">
+            <FontAwesomeIcon size="3x" icon={["fab", "facebook"]}/>
+          </div>
+          <div className="right-icon">
+            <FontAwesomeIcon size="3x" icon={["fab", "instagram"]}/>
+          </div>
+          <div className="right-icon">
+            <FontAwesomeIcon size="3x" icon={["fab", "tiktok"]}/>
+          </div>
       </div>
     </div>
   </div>
